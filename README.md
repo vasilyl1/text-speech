5944889ccf36c23953e99999809ae22ec67be90f
vasilyl1 <likhovaido@gmail.com>
2025-03-06 19:20:37 -0500

    speech to text model addition

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # text-speech
 simple text to speech generator using OpenAI tts-1 model
+audio to text generator using whisper-1 model
 
-Configure output file name, text prompt and API key in the environment variables, check .env.example on how to create .env file.
+Configure output file name, text prompt, input audio file name and API key in the environment variables, check .env.example on how to create .env file.
 
-I needed this functionality to design PBX prompts
+I needed this functionality to design PBX prompts and various other testing development tasks
--- index.js
+++ index.js
@@ -6,12 +6,21 @@ require('dotenv').config();
 const openai = new OpenAI({
     apiKey: process.env.OPENAI,
 });
+const argument = process.argv[2]; // Get the argument passed via command line
+
+const printInstructions = () => {
+    console.log("Usage: npm start [speech|toSpeech]\n");
+    console.log("Parameters:");
+    console.log("  speech      - Process the input prompt into speech (using INPUT environment variable)");
+    console.log("  text   - Process the audio file into text (using INPUTAUDIO environment variable)");
+};
+
 const speechFile = path.resolve(`./${process.env.FILE}`);
 
 const speech = async () => {
 
     try {
-        console.log(`Processing prompt: ${process.env.INPUT} \n Please be patient!`)
+        console.log(`Processing prompt: ${process.env.INPUT} \n Please be patient!`);
 
         const mp3 = await openai.audio.speech.create({
             model: "tts-1",
@@ -27,4 +36,26 @@ const speech = async () => {
     };
 };
 
-speech();
+const text = async () => {
+    try {
+        console.log(`Processing file: ${process.env.INPUTAUDIO} \n Please be patient!`);
+        const transcription = await openai.audio.transcriptions.create({
+            file: fs.createReadStream(process.env.INPUTAUDIO),
+            model: "whisper-1",
+        });
+        console.log(transcription.text);
+    } catch (err) {
+        console.error(err);
+    };
+};
+
+if (!argument) {
+    printInstructions();
+} else if (argument === 'speech') {
+    speech();
+} else if (argument === 'text') {
+    text();
+} else {
+    console.log("Invalid argument provided.");
+    printInstructions();
+};
